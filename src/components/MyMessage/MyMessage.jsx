0c24bbf11000341044436e91fb94827d3c93295d
siddharth <jsidd06@gmail.com>
2022-03-05 14:26:53 +0530

    add my message.jsx

--- src/components/MyMessage/MyMessage.jsx
+++ src/components/MyMessage/MyMessage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const MyMessage = () => {
+const MyMessage = ({message}) => {
+
+    if(message.attachments && message.attachments.length > 0) {
+        return (
+            <img src={message.attachment[0].file} alt={message.attachment}
+            className="message-image"
+            style={{float: 'right'}}
+             />
+        )
+    }
+
+
   return (
-    <div>MyMessage</div>
+    <div className="message" style={{float: 'right',marginRight: "18px",color:"white",backgroundColor:"#382A50"}}>{message.text}</div>
   )
 }
 
