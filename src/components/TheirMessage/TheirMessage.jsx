5b0812293872d79fa8158af09ad9f5cca0183212
siddharth <jsidd06@gmail.com>
2022-03-05 14:39:31 +0530

    their message

--- src/components/TheirMessage/TheirMessage.jsx
+++ src/components/TheirMessage/TheirMessage.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 
-function TheirMessage() {
+function TheirMessage({message,lastMessage}) {
+
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.userName !== message.sender.userName;
+
+
   return (
-    <div>TheirMessage</div>
-  )
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div
+          className="message-avatar"
+          style={{
+            backgroundImage: message.sender && `url($(message.sender.avatar))`,
+          }}
+        />
+      )}
+      {message.attachments && message.attachments.length > 0 ? (
+        <img
+          src={message.attachment[0].file}
+          alt={message.attachment}
+          className="message-image"
+          style={{ marginLeft: isFirstMessageByUser ? "4px" : "48px" }}
+        />
+      ) : (
+        <div
+          className="message"
+          style={{
+            float: "left",
+            marginLeft: isFirstMessageByUser ? "4px" : "48px",
+            color: "white",
+            backgroundColor: "#CABCDC",
+          }}
+        >
+          {message.text}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default TheirMessage
